c141030e36033d1f744359f96d2c3976227bcabf
Aleksandre <sandrokhosht@gmail.com>
2020-10-19 12:09:48 +0400

    fixed deleteUser

--- src/app/search-delete/search-delete.component.ts
+++ src/app/search-delete/search-delete.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistationService } from '../user-registation.service';
+import { User } from '../user'
 
 @Component({
   selector: 'app-search-delete',
@@ -14,15 +15,17 @@ export class SearchDeleteComponent implements OnInit {
   constructor(private service:UserRegistationService) { }
 
 
-public deleteUser(email:string){
- let resp= this.service.deleteUser(this.email);
+public deleteUser(user:User){
+ let resp= this.service.deleteUser(user.email);
  resp.subscribe((data)=>this.users.map=data);
+// console.log(user.email);
  this.ngOnInit();
 }
 
 public findUserByEmailId(email:string){
   let resp= this.service.getUserByEmail(this.email);
   resp.subscribe((data)=>this.users.map=data);
+  
  }
 
   ngOnInit() {
